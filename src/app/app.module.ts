acc43492dd909bcfea5f590e1ecc22c461cfadd8
muhammad-naveed <m.naveed.ch@gmail.com>
2017-07-29 22:22:34 +0500

    added required tabs to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {QuotesPage} from "../pages/quotes/quotes";
 import {QuotePage} from "../pages/quote/quote";
 import {FavoritesPage} from "../pages/favorites/favorites";
 import {SettingsPage} from "../pages/settings/settings";
+import {TabsPage} from "../pages/tabs/tabs";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {SettingsPage} from "../pages/settings/settings";
     QuotesPage,
     QuotePage,
     FavoritesPage,
-    SettingsPage
+    SettingsPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,8 @@ import {SettingsPage} from "../pages/settings/settings";
     QuotesPage,
     QuotePage,
     FavoritesPage,
-    SettingsPage
+    SettingsPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core'
+import {FavoritesPage} from "../favorites/favorites";
+import {LibraryPage} from "../library/library";
+
+@Component({
+  selector: 'page-tabs',
+  template: `
+  <ion-tabs>
+    <ion-tab [root]="favouritePage" tabTitle="Favourites" tabIcon="star"></ion-tab>
+    <ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"></ion-tab>
+  </ion-tabs>
+  `
+})
+export class TabsPage {
+  favouritePage = FavoritesPage;
+  libraryPage = LibraryPage;
+}
